5149852f1a2737667ab3d1365c9b433765bdc8fe
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 17:02:19 +0700

    2 cách khai báo map trong Go.

--- map.go
+++ map.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//Khai báo map
+	var myMap = make(map[string]int)
+	fmt.Println(myMap)
+
+	if myMap == nil {
+		fmt.Println("myMap = nil")
+	} else {
+		fmt.Println("!= nil")
+	}
+	fmt.Println()
+	var myMap1 map[string]int
+	fmt.Println(myMap1)
+
+	if myMap1 == nil {
+		fmt.Println("myMap1 = nil")
+	}
 }
